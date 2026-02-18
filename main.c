67dd334b25c76b3e5338af784432861d2cb63187
Kniffler <character.lumenit@gmail.com>
2026-02-18 14:04:13 +0000

    modified:   .gitignore 	modified:   .gitmodules 	renamed:    uthash -> AppShell/lcdspi/uthash 	modified:   CMakeLists.txt 	deleted:    FreeRTOS 	deleted:    FreeRTOS_Kernel_import.cmake 	deleted:    apps/CMakeLists.txt 	deleted:    apps/filos/filos.c 	deleted:    apps/iowiz/CMakeLists.txt 	deleted:    apps/iowiz/iowiz.c 	deleted:    apps/iowiz/iowiz.h 	deleted:    apps/manager.c 	deleted:    apps/manager.h 	deleted:    director/CMakeLists.txt 	deleted:    director/director.c 	deleted:    director/director.h 	deleted:    i2ckbd/CMakeLists.txt 	deleted:    i2ckbd/i2ckbd.c 	deleted:    i2ckbd/i2ckbd.h 	deleted:    include/FreeRTOSConfig.h 	deleted:    keyboard_define.h 	deleted:    lcdspi/CMakeLists.txt 	deleted:    lcdspi/fonts/font1.h 	deleted:    lcdspi/lcdspi.c 	deleted:    lcdspi/lcdspi.h 	modified:   main.c 	AppShell/CMakeLists.txt 	AppShell/FreeRTOS/ 	AppShell/FreeRTOS_Kernel_import.cmake 	AppShell/apps/ 	AppShell/i2ckbd/ 	AppShell/include/ 	AppShell/keyboard_define.h 	AppShell/lcdspi/CMakeLists.txt 	AppShell/lcdspi/fonts/ 	AppShell/lcdspi/lcdspi.c 	AppShell/lcdspi/lcdspi.h 	AppShell/pico_sdk_import.cmake 	AppShell/splitter/ 	config.h

--- main.c
+++ main.c
@@ -1,117 +1,238 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
-
-#include "FreeRTOS.h"
-#include "task.h"
-
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/clocks.h"
 #include "i2ckbd.h"
 #include "lcdspi.h"
-#include "keyboard_define.h"
-// #include "manager.h"
-#include "director.h"
+#include <hardware/flash.h>
+#include <hardware/watchdog.h>
+#include "config.h"
 
+#include "blockdevice/sd.h"
+#include "filesystem/fat.h"
+#include "filesystem/vfs.h"
+/*
+#include "text_directory_ui.h"
+#include "key_event.h"
+*/
+// Vector and RAM offset
+#if PICO_RP2040
+#define VTOR_OFFSET M0PLUS_VTOR_OFFSET
+#define MAX_RAM 0x20040000
 
-#define KBD_TASK_PRIORITY	12
+#elif PICO_RP2350
+#define VTOR_OFFSET M33_VTOR_OFFSET
+#define MAX_RAM 0x20080000
+#endif
 
-const unsigned int LEDPIN = 25;
-
-
-void all_init();
-
-static void kbd_handler_task(void *data);
-bool is_printable_key(int key) { return key>31&&key<127; };
-
-void main(void)
+bool sd_card_inserted(void)
 {
-	app_focused = 0;
-	all_init();
+	return !gpio_get(SD_DET_PIN);
+}
+
+bool fs_init(void)
+{
+	blockdevice_t *sd = blockdevice_sd_create(spi0,
+		SD_MOSI_PIN,
+		SD_MISO_PIN,
+		SD_SCLK_PIN,
+		SD_CS_PIN,
+		125000000 / 2 / 4, // 15.6MHz
+	true);
 	
-	/*TaskHandle_t kbd_handler = NULL;
-	BaseType_t xReturned;
-	xReturned =*/
-	xTaskCreate(kbd_handler_task, "kbd_handler", 128, (void *) NULL, KBD_TASK_PRIORITY, NULL);
-	//, &kbd_handler);
+	filesystem_t *fat = filesystem_fat_create();
 
-	dir_clear();
-
-	vTaskStartScheduler();
+	int err = fs_mount("/sd", fat, sd);
+	if (err != -1) { return true; }
+	
+	err = fs_format(fat, sd);
+	if (err == -1) { return false; }
+	
+	err = fs_mount("/sd", fat, sd);
+	if (err == -1) { return false; }
 }
 
-static void kbd_handler_task(void *data)
+static bool __not_in_flash_func(is_same_as_existing_program)(FILE *fp)
 {
-	int key;
-	for(;;)
+	uint8_t buffer[FLASH_SECTOR_SIZE] = {0};
+	size_t program_size = 0;
+	size_t len = 0;
+	while ((len = fread(buffer, 1, sizeof(buffer), fp)) > 0)
 	{
-		key = read_i2c_kbd();
-		if(key == -1) { continue; }
+		uint8_t *flash = (uint8_t *)(XIP_BASE + SD_BOOT_FLASH_OFFSET + program_size);
+		if (memcmp(buffer, flash, len) != 0)
+		{ return false; }
 		
-		// if(!(app_focused)||directive_write_referal)
-		if(!(app_focused))
-		{
-			directive_key_press(key); 
-		}
+		program_size += len;
 	}
-	vTaskDelete(NULL);
+	return true;
 }
 
-
-
-void all_init()
+// This function must run from RAM since it erases and programs flash memory
+static bool __not_in_flash_func(load_program)(const char *filename)
 {
-	set_sys_clock_khz(133000, true);
+	FILE *fp = fopen(filename, "r");
+	if (fp == NULL) { return false; }
+
+	if (is_same_as_existing_program(fp))
+	{
+		return true; // save ourselves this workload
+	}
+
+	// Check file size to ensure it doesn't exceed the available flash space
+	if (fseek(fp, 0, SEEK_END) == -1) { fclose(fp); return false; }
+
+	long file_size = ftell(fp);
+
+	if (file_size <= 0) { fclose(fp); return false; }
+	if (file_size > MAX_APP_SIZE) { fclose(fp); return false; }
+	if (fseek(fp, 0, SEEK_SET) == -1) { fclose(fp); return false; }
+
+	
+	size_t program_size = 0;
+	
+	uint8_t first_page = 0;
+	uint8_t buffer[FLASH_SECTOR_SIZE] = {0};
+	
+	size_t len = 0;
+
+	// Erase and program flash in FLASH_SECTOR_SIZE chunks
+	while ((len = fread(buffer, 1, sizeof(buffer), fp)) > 0)
+	{
+		// Ensure we don't write beyond the application area
+		if ((program_size + len) > MAX_APP_SIZE)
+		{
+			fclose(fp);
+			return false;
+		}
+		
+		uint32_t ints = save_and_disable_interrupts();
+		flash_range_erase(SD_BOOT_FLASH_OFFSET + program_size, FLASH_SECTOR_SIZE);
+
+		/* Don't program the first page, and save it.
+			This way we prevent launching apps that have not yet been fully loaded into flash.
+		*/
+		if(program_size != 0)
+		{
+			flash_range_program(SD_BOOT_FLASH_OFFSET + program_size, buffer, len);
+		}else{
+			flash_range_program(SD_BOOT_FLASH_OFFSET + program_size, 0, len);	// In case there is a valid vector table stored
+			first_page = *buffer;
+		}
+
+		restore_interrupts(ints);
+
+		program_size += len;
+	}
+	
+	uint32_t ints = save_and_disable_interrupts();
+	flash_range_program(SD_BOOT_FLASH_OFFSET, &first_page, len);
+	restore_interrupts(ints);
+	
+	fclose(fp);
+	return true;
+}
+
+// This function jumps to the application entry point
+// It must update the vector table and stack pointer before jumping
+void __not_in_flash_func(launch_application_from)(uint32_t *app_location)
+{
+	// https://vanhunteradams.com/Pico/Bootloader/Bootloader.html
+	uint32_t *new_vector_table = app_location;
+	volatile uint32_t *vtor = (uint32_t *)(PPB_BASE + VTOR_OFFSET);
+	*vtor = (uint32_t)new_vector_table;
+	asm volatile(
+		"msr msp, %0\n"
+		"bx %1\n"
+		:
+		: "r"(new_vector_table[0]), "r"(new_vector_table[1])
+		:);
+}
+
+// Check if a valid application exists in flash by examining the vector table
+static bool is_valid_application(uint32_t *app_location)
+{
+	// Check that the initial stack pointer is within a plausible RAM region (assumed range for Pico: 0x20000000 to 0x20040000)
+	uint32_t stack_pointer = app_location[0];
+	if (stack_pointer < 0x20000000 || stack_pointer > MAX_RAM)
+	{
+		return false;
+	}
+
+	// Check that the reset vector is within the valid flash application area
+	uint32_t reset_vector = app_location[1];
+	if (reset_vector < (0x10000000 + SD_BOOT_FLASH_OFFSET) || reset_vector > (0x10000000 + PICO_FLASH_SIZE_BYTES))
+	{
+		return false;
+	}
+	return true;
+}
+
+int load_and_launch_firmware_by_path(const char *path)
+{
+	// Attempt to load the application from the SD card
+	// bool load_success = load_program(FIRMWARE_PATH);
+	bool load_success = load_program(path);
+
+	// Get the pointer to the application flash area
+	uint32_t *app_location = (uint32_t *)(XIP_BASE + SD_BOOT_FLASH_OFFSET);
+
+	// Check if the app in flash is valid
+	bool has_valid_app = is_valid_application(app_location);
+
+	// Loading was valid and successfull
+	if (load_success || has_valid_app)
+	{
+		// Small delay to allow printf to complete
+		sleep_ms(100);
+		launch_application_from(app_location);
+	}
+	else
+	{
+		sleep_ms(2000);
+		// Trigger a watchdog reboot
+		watchdog_reboot(0, 0, 0);
+	}
+
+	// We should never reach here
+	while (1)
+	{
+		tight_loop_contents();
+	}
+}
+
+void final_selection_callback(const char *path)
+{
+	// Trigger firmware loading with the selected path
+
+	char status_message[128];
+	const char *extension = ".bin";
+	size_t path_len = strlen(path);
+	size_t ext_len = strlen(extension);
+
+	if (path_len < ext_len || strcmp(path + path_len - ext_len, extension) != 0)
+	{
+		snprintf(status_message, sizeof(status_message), "Err: FILE is not a .bin file");
+		return;
+	}
+
+	snprintf(status_message, sizeof(status_message), "SEL: %s", path);
+
+	sleep_ms(200);
+
+	load_and_launch_firmware_by_path(path);
+}
+
+int main()
+{
 	stdio_init_all();
 
-	init_i2c_kbd();
+	// Initialize SD card detection pin
+	gpio_init(SD_DET_PIN);
+	gpio_set_dir(SD_DET_PIN, GPIO_IN);
+	gpio_pull_up(SD_DET_PIN); // Enable pull-up resistor
+
 	lcd_init();
-	
-	gpio_init(LEDPIN);
-	gpio_set_dir(LEDPIN, GPIO_OUT);
-
-	gpio_put(LEDPIN, 1);
-	sleep_ms(1600);
-	
-	lcd_set_colours(WHITE, BLACK);
-	// lcd_set_colours(RGB(128, 0, 32), BLACK);
-	sleep_ms(200);
 	lcd_clear();
-	sleep_ms(200);
-	
-	dir_init();
 }
-
-
-
-
-/*
-* These functions are requried for FreeRTOS to work in static memory mode.
-*/
-
-#if configSUPPORT_STATIC_ALLOCATION
-static StaticTask_t idle_task_tcb;
-static StackType_t idle_task_stack[mainIDLE_TASK_STACK_DEPTH];
-void vApplicationGetIdleTaskMemory(
-    StaticTask_t **ppxIdleTaskTCBBuffer,
-    StackType_t **ppxIdleTaskStackBuffer,
-    uint32_t *pulIdleTaskStackSize
-) {
-    *ppxIdleTaskTCBBuffer = &idle_task_tcb;
-    *ppxIdleTaskStackBuffer = idle_task_stack;
-    *pulIdleTaskStackSize = mainIDLE_TASK_STACK_DEPTH;
-}
-
-static StaticTask_t timer_task_tcb;
-static StackType_t timer_task_stack[configMINIMAL_STACK_SIZE];
-void vApplicationGetTimerTaskMemory(
-    StaticTask_t **ppxTimerTaskTCBBuffer,
-    StackType_t **ppxTimerTaskStackBuffer,
-    uint32_t *pulTimerTaskStackSize
-) {
-    *ppxTimerTaskTCBBuffer = &timer_task_tcb;
-    *ppxTimerTaskStackBuffer = timer_task_stack;
-    *pulTimerTaskStackSize = configMINIMAL_STACK_SIZE;
-}
-#endif
